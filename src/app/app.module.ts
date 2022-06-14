123e9ed5e8a31e03979edc900347d1ed4b7eef33
Sammy <rockarim@hotmail.com>
2022-06-14 18:16:09 +0000

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './_shared/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './_shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
