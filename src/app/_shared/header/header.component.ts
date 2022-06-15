522b919ba4eb9664c315209287220a7c3f655f29
sammyach <sammyachman@gmail.com>
2022-06-15 18:03:48 +0000

    make site mobile responsive

--- src/app/_shared/header/header.component.ts
+++ src/app/_shared/header/header.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  toggleClass: any;
+  opened = false;
+  showMenu: any;
+  showMbMenu: any = 'hide';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggle(){
+    console.log('toggle...', this.opened);
+
+    this.showMbMenu = 'show';
+    // this.opened = !this.opened;
+    // this.showMenu = this.opened ? 'show' : 'hide';
+  }
+
+  hideMenu(){
+    this.showMbMenu = 'hide';
+  }
 }
