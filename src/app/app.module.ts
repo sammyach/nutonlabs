236acb3a3e2c20cfa00c60b384b496c501e3150a
sammyach <sammyachman@gmail.com>
2022-06-17 22:08:04 +0000

    added products component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './_shared/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './_shared/footer/footer.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
