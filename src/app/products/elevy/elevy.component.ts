c735f9de69bf6915cdfdb8a275dcf5bb5e3df3c3
sammyach <sammyachman@gmail.com>
2022-07-15 20:45:18 +0000

    added elevy calculator page

---
+++ src/app/products/elevy/elevy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-elevy',
+  templateUrl: './elevy.component.html',
+  styleUrls: ['./elevy.component.scss']
+})
+export class ElevyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
