ee49cabc19d5634c66edcb5386bb5b482176a6ba
sammyach <sammyachman@gmail.com>
2022-07-18 09:56:18 +0000

    added css animations to improve ux

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, DoCheck, ElementRef, HostListener, NgZone, OnInit, Renderer2 } from '@angular/core';
 import * as data from './../data.json'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit, DoCheck {
 
+  childElements!: NodeListOf<Element>;
   products: any = (data as any).default;
-  constructor() { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2, private ngZone: NgZone) {
+  }
 
   ngOnInit(): void {
-    console.log('home => ',this.products);
+
+    // this.ngZone.runOutsideAngular(()=>{
+    //   this.onWindowScroll();
+    // })
+
+    // console.log('home => ',this.products);
+    // this.wz.scrollY$.subscribe(r => {
+    //   // console.log('zcroll level', (r - this.getScrollHeight()));
+
+    // })
   }
+
+  ngAfterViewInit(): void {
+    this.childElements = (<HTMLElement>this.elementRef.nativeElement).querySelectorAll('.title');
+  }
+
+  ngDoCheck(): void {
+    console.log('checking changez');
+
+
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll(){
+    // console.log('zcroll', $event);
+    // this.wz.updateScrollY(this.getYPosition($event))
+
+
+    if(this.childElements?.length > 0){
+      for(let i=0; i<this.childElements.length; i++){
+        const windowHeight = window.innerHeight;// height of the viewport.
+        const elementTop = this.childElements.item(i).getBoundingClientRect().top;// distance from the top of the viewport
+        // const elementVisible = 650;
+
+        if(elementTop < windowHeight){
+          this.renderer.addClass(this.childElements.item(i), 'reveal')
+        }else{
+          this.renderer.removeClass(this.childElements.item(i), 'reveal')
+        }
+      }
+    }
+  }
+
+  // getYPosition(e: Event){
+  //   // console.log('xxx => ',(e?.target as Element)?.scrollTop);
+
+  //   return (e?.target as Element)?.scrollTop;
+  // }
+
+  // getScrollHeight(): number{
+  //   return Math.max(
+  //     document.body.scrollHeight, document.documentElement.scrollHeight,
+  //     document.body.offsetHeight, document.documentElement.offsetHeight,
+  //     document.body.clientHeight, document.documentElement.clientHeight
+  //   );
+  // }
 
 }
